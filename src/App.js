77716b8a6bd04a0b61310ef06f2bc63f94b8d2ff
Nancy <nancy62924@gmail.com>
2022-03-07 18:58:51 +0800

    add game UI interface

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+/**
+ * Challenge: Using hooks, track the state of the text in the textarea on every keystroke
+ * To verify it's working, you could just console.log the state on every change
+ */
+import React, { useState } from "react";
 
 function App() {
+  const [text, setText] = useState("");
+
+  function handleText(e) {
+    setText(e.target.value);
+  }
+
+  console.log(text);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <h1>How fast do you type?</h1>
+      <textarea value={text} onChange={handleText} className="textarea" />
+      <h4>Time remaining: </h4>
+      <button>Start</button>
+      <h1>Word Count:</h1>
+    </main>
   );
 }
 
